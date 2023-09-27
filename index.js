558589fbcd2879bfd908351115e5c5965e12146b
Michael Ingram <michael.ingram@flybreeze.com>
2023-09-27 16:50:15 -0500

    update url

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const axios = require('axios');
 const EventEmitter = require('events');
 
-async function checkOnlineStatus (urlToPing = 'https://www.google.com', maxRetries = 3, retryInterval = 1000)  {
+async function checkOnlineStatus (urlToPing = 'https://httpbin.org/status/200', maxRetries = 3, retryInterval = 3000)  {
     let retries = 0;
 
     while (retries < maxRetries) {
         try {
-            // Send a GET request to Google's homepage
+            // Send a GET request to check online status
             await axios.get(urlToPing);
             return true; // Success, you are online!
         } catch (error) {
@@ -24,10 +24,10 @@ class OnlineStatusChecker extends EventEmitter {
         super();
 
         const {
-            checkInterval = 5000,
+            checkInterval = 10000,
             maxRetries = 3,
-            urlToPing = 'https://www.google.com',
-            retryInterval = 1000
+            urlToPing = 'https://httpbin.org/status/200',
+            retryInterval = 3000
         } = options;
 
         this.isOnline = false; // Initial status
